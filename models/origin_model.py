ff07248b752000c2fa451017ee540ebee2a80d51
Markin-Wang <cserwj@gmail.com>
2023-10-15 16:04:55 +0100

    put gelu before layer norm

--- models/origin_model.py
+++ models/origin_model.py
@@ -45,8 +45,8 @@ class RRGModel(nn.Module):
 
         self.att_embed = nn.Sequential(
             nn.Linear(self.att_feat_size, self.hidden_size),
-            nn.GELU(),
             *([nn.LayerNorm(self.hidden_size)] if self.use_ln else []),
+            nn.GELU(),
             *([nn.Dropout(p=self.drop_prob_lm)] if self.use_dropout else []),
         )
         self.att_embed.apply(init_weights)
