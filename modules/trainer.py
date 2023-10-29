add gather to ensure the correct evaluation metric calculation

modules/trainer.py
@@ -12,7 +12,7 @@ from modules.weighted_mesloss import Weighted_MSELoss
 from torch.cuda.amp import GradScaler, autocast
 from timm.utils import AverageMeter
 import torch.distributed as dist
-from .utils import clip_grad_norm_, calculate_auc, get_region_mask
+from .utils import clip_grad_norm_, calculate_auc, get_region_mask, gather_preds_and_gts
 import numpy as np
 from modules.beam_search import BeamSearch
 from modules.loss import AsymmetricLoss, AsymmetricLossOptimized
@@ -51,6 +51,7 @@ class BaseTrainer(object):
         self.early_stop = config['early_stop']
         self.local_rank = config['local_rank']
         self.att_pad_idx = config['att_pad_idx']
+        self.pad_idx = config['pad_idx']
         # if args.debug:
         #     for submodule in model.modules():
         #         submodule.register_forward_hook(nan_hook)
@@ -61,6 +62,7 @@ class BaseTrainer(object):
         #     module.register_backward_hook(get_activations(name))
         self.optimizer = optimizer
         self.scaler = GradScaler(enabled=self.use_amp, init_scale=256)
+        self.max_seq_length = config['max_seq_length']
 
         self.region_cls = config['region_cls']
         self.clip_option = config['clip_option']
@@ -144,13 +146,14 @@ class BaseTrainer(object):
                     log.update(self._valid(epoch, 'test'))
                 # save logged informations into log dict
                 # synchronize log in different gpu
-                log = self._synchronize_data(log)
+                log = self._broadcast_data(log)
 
                 log['epoch'] = epoch
 
                 # print logged informations to the screen
                 for key, value in log.items():
                     self.logger.info('\t{:15s}: {}'.format(str(key), value))
+
 
                 improved = self._record_best(log)
 
@@ -199,6 +202,16 @@ class BaseTrainer(object):
         values = reduce_tensor(values)
         log.update({k: v.item() for k, v in zip(keys, values)})
         return log
+
+    def _broadcast_data(self, log):
+        # pairs = [[k, v] for k, v in log.items()]
+        # keys = [x[0] for x in pairs]
+        if dist.get_rank() == 0:
+            object = [log]
+        else:
+            object = [None]
+        torch.distributed.broadcast_object_list(object,src=self.local_rank)
+        return object[0]
 
     def _print_best_to_file(self):
         crt_time = time.asctime(time.localtime(time.time()))
@@ -421,7 +434,7 @@ class Trainer(BaseTrainer):
                         #                                    data['box_labels'].to(device, non_blocking=True), \
                         #                                    data['region_labels'].to(device, non_blocking=True)
                         region_labels = batch_dict['region_labels']
-                        region_masks = get_region_mask(region_labels).cpu()
+                        region_masks = get_region_mask(region_labels)
                         region_labels = region_labels[region_masks]
 
                     with autocast(dtype=torch.float16):
@@ -434,11 +447,11 @@ class Trainer(BaseTrainer):
                                 val_region_cls_loss = self.region_cls_criterion(region_logits, region_labels)
                                 val_region_cls_losses.update(val_region_cls_loss.item())
                                 if len(region_preds) > 0:
-                                    region_preds = torch.cat((region_preds, region_probs.cpu()), dim=0)
-                                    region_targets = torch.cat((region_targets, region_labels.cpu()), dim=0)
+                                    region_preds = torch.cat((region_preds, region_probs), dim=0)
+                                    region_targets = torch.cat((region_targets, region_labels), dim=0)
                                 else:
-                                    region_preds = region_probs.cpu()
-                                    region_targets = region_labels.cpu()
+                                    region_preds = region_probs
+                                    region_targets = region_labels
 
                         if self.att_cls:
                             att_probs_record = output['att_probs_record']
@@ -449,36 +462,52 @@ class Trainer(BaseTrainer):
                                         attribute_preds[box_label].append(
                                             att_probs_record[bs_id][box_label][:cgnome_id2cat[box_label]].unsqueeze(0))
                                         attribute_targets[box_label].append(
-                                            attribute_labels[bs_id][box_label])
+                                            attribute_labels[bs_id][box_label].to(device))
 
                         if self.use_new_bs:
                             output = self.beam_search.caption_test_step(self.model.module, batch_dict=output)
                         else:
                             output = self.beam_search.sample(self.model.module, patch_feats=output['encoded_img_feats'])
-
-                    reports = self.tokenizer.decode_batch(output['preds'].numpy())
-                    ground_truths = self.tokenizer.decode_batch(batch_dict['text'][:, 1:].cpu().numpy())
-                    val_res.extend(reports)
-                    val_gts.extend(ground_truths)
+                    val_res.append(output['preds'])
+                    val_gts.append(self._pad(batch_dict['text'][:, 1:]))
+                    # reports = self.tokenizer.decode_batch(output['preds'].numpy())
+                    # ground_truths = self.tokenizer.decode_batch(batch_dict['text'][:, 1:].cpu().numpy())
+                    # val_res.extend(reports)
+                    # val_gts.extend(ground_truths)
                     memory_used = torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)
                     # pbar.set_postfix(ce_ls=val_ce_losses / (batch_idx + 1), cls_ls=val_img_cls_losses / (batch_idx + 1),
                     #                  mse_ls=val_mse_losses / (batch_idx + 1), mem=f'mem {memory_used:.0f}MB')
                     pbar.update()
-                val_met = self.metric_ftns({i: [gt] for i, gt in enumerate(val_gts)},
-                                           {i: [re] for i, re in enumerate(val_res)})
-                log.update(**{f'{split}_' + k: v for k, v in val_met.items()})
+                val_res, val_gts = torch.cat(val_res, dim=0), torch.cat(val_gts, dim=0)
+                val_res, val_gts = gather_preds_and_gts(val_res, val_gts)
+                if dist.get_rank() == self.local_rank:
+                    val_res, val_gts = torch.cat(val_res, dim=0), torch.cat(val_gts, dim=0)
+                    val_res, val_gts = self.tokenizer.decode_batch(val_res.cpu().numpy()), self.tokenizer.decode_batch(
+                        val_gts.cpu().numpy())
+                    val_met = self.metric_ftns({i: [gt] for i, gt in enumerate(val_gts)},
+                                               {i: [re] for i, re in enumerate(val_res)})
+                    log.update(**{f'{split}_' + k: v for k, v in val_met.items()})
+                    val_res, val_gts = None, None
+
                 if split == 'val':
                     log.update({'val_ce_loss': val_ce_losses.avg})
+
                 if self.region_cls:
-                    region_auc = calculate_auc(preds=region_preds, targets=region_targets)
-                    log.update({f'{split}_region_auc': region_auc, f"{split}_rg_loss": val_region_cls_losses.avg})
+                    region_preds, region_targets = gather_preds_and_gts(region_preds, region_targets)
+                    if dist.get_rank() == self.local_rank:
+                        region_preds, region_targets = torch.cat(region_preds,dim=0), torch.cat(region_targets,dim=0)
+                        region_auc = calculate_auc(preds=region_preds.cpu().numpy(), targets=region_targets.cpu().numpy())
+                        log.update({f'{split}_region_auc': region_auc, f"{split}_rg_loss": val_region_cls_losses.avg})
                 if self.att_cls:
                     att_aucs = []
                     for key in attribute_preds.keys():
                         try:
-                            att_auc = calculate_auc(preds=torch.cat(attribute_preds[key], dim=0),
-                                                 targets=torch.cat(attribute_targets[key], dim=0))
-                            att_aucs.append(att_auc)
+                            att_preds, att_gts = torch.cat(attribute_preds[key], dim=0), torch.cat(attribute_targets[key], dim=0)
+                            att_preds, att_gts = gather_preds_and_gts(att_preds, att_gts)
+                            if dist.get_rank() == self.local_rank:
+                                att_preds, att_gts = torch.cat(att_preds, dim=0), torch.cat(att_gts,dim=0)
+                                att_auc = calculate_auc(preds=att_preds.cpu().numpy(),targets=att_gts.cpu().numpy())
+                                att_aucs.append(att_auc)
                         except  ValueError:
                             self.logger.info(f'Att calculation on category {categories[key]} fails.')
                             continue
@@ -486,11 +515,17 @@ class Trainer(BaseTrainer):
                     log.update({f'{split}_att_auc': att_auc})
         return log
 
+    def _pad(self,data):
+        padded_data = torch.full((len(data),self.max_seq_length-1),self.pad_idx,device=data[0].device)
+        for i,cur_data in enumerate(data):
+            padded_data[i,:cur_data.size(0)] = cur_data
+        return padded_data
+
     def test(self):
         self.logger.info('Starting evaluating the best checkpoint in test set.')
         log = {}
         log.update(self._valid(0, 'test'))
-        log = self._synchronize_data(log)
+        #log = self._synchronize_data(log)
         self.logger.info('The result for the best performed models in test set.')
         for key, value in log.items():
             self.logger.info('\t{:15s}: {}'.format(str(key), value))