afc258163667bdaa31be1bd9355045bc594c6aaa
Markin-Wang <cserwj@gmail.com>
2023-10-29 21:21:02 +0000

    change region/att labels to long when cal auc

--- modules/trainer.py
+++ modules/trainer.py
@@ -497,7 +497,6 @@ class Trainer(BaseTrainer):
                     region_preds, region_targets = gather_preds_and_gts(region_preds, region_targets)
                     if dist.get_rank() == self.local_rank:
                         region_preds, region_targets = torch.cat(region_preds,dim=0), torch.cat(region_targets,dim=0).long()
-                        print(torch.unique(region_targets),region_targets.dtype)
                         region_auc = calculate_auc(preds=region_preds.cpu().numpy(), targets=region_targets.cpu().numpy())
                         log.update({f'{split}_region_auc': region_auc, f"{split}_rg_loss": val_region_cls_losses.avg})
                 if self.att_cls:
